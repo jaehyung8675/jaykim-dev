4c57d72927f06e7b319163aea9f4edc68a90b814
jaehyung8675 <jaehyung8675@gmail.com>
2023-03-01 10:08:41 -0600

    update

--- app/js/script.js
+++ app/js/script.js
@@ -4,29 +4,40 @@
 const header = document.querySelector('.header');
 const nav = document.querySelector('nav');
 const logo = document.querySelector('.logo');
-const navLinks = document.querySelector('ul');
+const navLinks = document.querySelector('.nav__links');
 const about = document.getElementById('about');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const experienceMenu = document.querySelector('menu');
 const experienceTabs = document.querySelectorAll('.btn--experience');
 const experienceContents = document.querySelectorAll('.article--experience');
+const projects = document.querySelector('.projects');
+const inputName = document.querySelector('.form__input--name');
+const inputEmail = document.querySelector('.form__input--email');
+const inputTextarea = document.querySelector('.form__input--textarea');
+const inputBtn = document.querySelector('.form__input--btn');
 
 //// Functions
-// Sticky navigation - Interaction Observer API
+// Reveal sections
+// const allNavLinks = document.querySelectorAll('.nav__link');
 
-// const obsCallback = function (entries, observer) {
-//   entries.forEach((entry) => console.log(entry));
+// const revealNav = function (entries, observer) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) return;
+
+//   entry.target.classList.remove('link--hidden');
 // };
 
-// const obsOptions = {
+// const navObserver = new IntersectionObserver(revealNav, {
 //   root: null,
 //   threshold: 0,
-// };
+// });
 
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// allNavLinks.forEach((link) => {
+//   navObserver.observe(link);
+//   link.classList.add('link--hidden');
+// });
 
-// observer.observe(about);
-
+// Sticky navigation - Interaction Observer API
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
@@ -48,7 +59,7 @@ headerObserver.observe(header);
 // Section navigation
 navLinks.addEventListener('click', function (e) {
   e.preventDefault();
-  if (e.target.classList.contains('link')) {
+  if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
@@ -80,19 +91,34 @@ experienceMenu.addEventListener('click', function (e) {
     .classList.add('active');
 });
 
-//
-function sendEmail() {
-  const serviceID = 'service_2v7h2lg';
-  const templateID = 'template_wtxthue';
+// Project fase animation
+const handleHover = function (e) {
+  if (
+    e.target.classList.contains('project__img') ||
+    e.target.classList.contains('project__caption')
+  ) {
+    const link = e.target.closest('figure');
+    const siblings = link.closest('.projects').querySelectorAll('figure');
 
-  const emailData = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
-  };
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
 
-  emailjs.send('service_2v7h2lg', 'template_wtxthue', emailData).then((res) => {
-    console.log(emailData.name);
-    // alert('Your message sent successfully!' + res.status);
-  });
-}
+projects.addEventListener('mouseover', handleHover.bind(0.3));
+projects.addEventListener('mouseout', handleHover.bind(1));
+
+// Contact form submit
+// Nedd to find how to clean input value after submiting
+
+// const form = document.querySelector('.form');
+// inputBtn.addEventListener('click', function (e) {
+//   // e.preventDefault();
+//   // inputName.value = inputEmail.value = inputTextarea.value = '';
+//   form.reset();
+// });
+
+// function submitReset() {
+//   inputName.value = inputEmail.value = inputTextarea.value = '';
+// }
